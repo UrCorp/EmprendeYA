8d257a4e061910cdf333d79aa2c8b9b6a0bd718f
Michael Serrato <programador-experto97@hotmail.com>
2016-08-01 00:06:23 -0500

    Detalles de diseño #5

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,13 @@
 $(function() {
+  var $navbar = $('#navbar'),
+      $btnOpenNavbar = $('#btn-open-navbar'),
+      $flashOverlayModal = $('#flash-overlay-modal');
+  
+  $btnOpenNavbar.collapse();
+  $flashOverlayModal.modal();
   $('a.anchorLink').anchorAnimate();
-  $('#btn-open-navbar').collapse();
-  $('#flash-overlay-modal').modal();
+
+  $('#header .app-navbar ul li a').click(function(event) {
+    $navbar.collapse('hide');
+  });
 });

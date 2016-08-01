509cddaa8cfe55099031c1f31fb953ad5975976d
Michael Serrato <programador-experto97@hotmail.com>
2016-08-01 17:06:33 -0500

    [IndexPage] Jumbotron & Quotes versión purpura.

--- resources/views/site/welcome/index.blade.php
+++ resources/views/site/welcome/index.blade.php
@@ -15,17 +15,17 @@
       </ol>
       <div class="carousel-inner" role="listbox">
         <div class="item active">
-          <img src="{!! asset('public/assets/img/carousel-1.jpg') !!}" alt="Emprendedor 1">
+          <img src="{!! asset('public/assets/img/carousel-1.png') !!}" alt="Emprendedor 1">
           <div class="carousel-caption">
           </div>
         </div>
         <div class="item">
-          <img src="{!! asset('public/assets/img/carousel-2.jpg') !!}" alt="Emprendedor 2">
+          <img src="{!! asset('public/assets/img/carousel-2.png') !!}" alt="Emprendedor 2">
           <div class="carousel-caption">
           </div>
         </div>
         <div class="item">
-          <img src="{!! asset('public/assets/img/carousel-3.jpg') !!}" alt="Emprendedor 3">
+          <img src="{!! asset('public/assets/img/carousel-3.png') !!}" alt="Emprendedor 3">
           <div class="carousel-caption">
           </div>
         </div>
